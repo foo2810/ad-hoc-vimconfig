9363c129bffc39ae9527796f8a6032b3ac37b88b
hogedamari <29653357+foo2810@users.noreply.github.com>
2023-08-22 21:53:22 +0900

    bug fixed: install.sh executed using pipes output endless error messages
    If ~/.vimrc already exists,
    install.sh asked you to choose whether or not to overwrite it.
    If install.sh is piped to bash,
    the script hung because it did not accept input from the terminal.

--- install.sh
+++ install.sh
@@ -9,30 +9,16 @@
 VIMRC_PATH=$(realpath ~/.vimrc)
 VIMRC_URL="https://raw.githubusercontent.com/foo2810/ad-hoc-vimconfig/main/.vimrc"
 
-select_yes_no() {
-    local ans
-    local yes_pattern
-    local no_pattern
-
-    yes_pattern="^yes$|^YES$|^Yes$|^y$|^Y$"
-    no_pattern="^no$|^NO$|^No$|^n$|^N$"
-    while true; do
-        read ans
-        if [[ $ans =~ $yes_pattern ]]; then
-            return 0
-        fi
-        if [[ $ans =~ $no_pattern ]]; then
-            return 1
-        fi
-        echo "Invalid answer. Please retry."
-    done
-}
-
 if [ -f $VIMRC_PATH ]; then
     echo "$VIMRC_PATH already exists"
-    echo "Are you sure to override ${VIMRC_PATH} ?"
-    if ! select_yes_no; then
-        exit 0
+    if [ -z "$FORCE" ] || [ $FORCE -ne 1 ]; then
+        cat << EOF
+To override cofing, execute following command:
+curl -fsSL https://raw.githubusercontent.com/foo2810/ad-hoc-vimconfig/main/install.sh | FORCE=1 bash
+
+Abort
+EOF
+        exit 1
     fi
 fi
 
